de602fc48948f84aeaa78783b9104d8fc6dd67fe
Timeree <s351@qq.com>
2025-12-25 18:10:35 +0800

    629bdcc0

--- README.md
+++ README.md
@@ -4,7 +4,5 @@
 自己用的仓库，包含一些私有工具
 
 ```bash
-  cd ~
-  git clone https://github.com/Timeree/Timeree.git
+  curl -sL https://raw.githubusercontent.com/Timeree/Timeree/main/init.sh | bash
 ```
-
---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+(
+  cd ~
+
+  git clone https://github.com/Timeree/Timeree.git
+
+  echo 'export PATH=${PATH}:~/Timeree/tools' >> ~/.bashrc
+
+  initghcs
+)
+
+# curl -sL https://raw.githubusercontent.com/Timeree/Timeree/main/init.sh | bash
+
