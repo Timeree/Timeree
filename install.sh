8c557457c49df383556c3a9d5c41de3155ba6532
Timeree <s351@qq.com>
2026-04-26 02:25:16 +0800

    f06b733b

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@
     git clone https://github.com/Timeree/Timeree.git "$HOME/Timeree"
   fi
 
-  : 'export PATH="${PATH}:$HOME/Timeree/tools"' > "$PREFIX/etc/profile.d/timeree.sh"
+  echo 'export PATH="${PATH}:$HOME/Timeree/tools"' > "$PREFIX/etc/profile.d/timeree.sh"
   
   chmod 644 "$PREFIX/etc/profile.d/timeree.sh"
 
